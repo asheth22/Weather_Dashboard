02616e3cec2fbd32f8a9b8427212bc564ba13c4b
Anjali Sheth <anjali.sheth@otis.com>
2021-01-07 18:06:57 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,106 @@
+var now = dayjs();
+var today = now.format("MM/DD/YYYY");
+console.log("Today: ", today);
+function buildQueryURL() {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+    var queryParams = { appid: "b0c91232e2976ef85bbb49769bd7a2b3" };
+
+    queryParams.q = $("#search-term")
+        .val()
+        .trim();
+
+    // Logging the URL so we have access to it for troubleshooting
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
+    console.log(queryURL + $.param(queryParams));
+    return queryURL + $.param(queryParams);
+}
+
+function updatePage(WeatherData) {
+
+    console.log("Inside updatePage function");
+    console.log(WeatherData);
+
+
+    var temperature = (WeatherData.current.temp - 273.15) * 1.80 + 32;
+    temperature = temperature.toFixed(2) + '\xB0F';
+    console.log("Temperature: ", temperature);
+
+    var humidity = WeatherData.current.humidity + '%';
+    console.log("Humidity: ", humidity);
+
+    var speed = WeatherData.current.wind_speed + " MPH";
+    console.log("Wind Speed: ", speed);
+
+    var uvi = WeatherData.current.uvi;
+    console.log("UV Indes: ", uvi);
+
+    for (i = 1; i < 6; i++) {
+
+        var newDate = addDays(today, i);
+
+        console.log("Date: ", newDate);
+        temperature = (WeatherData.daily[i].temp.day - 273.15) * 1.80 + 32;
+        temperature = temperature.toFixed(2) + '\xB0F';
+        console.log("Temperature: ", temperature);
+
+        humidity = WeatherData.daily[i].humidity + '%';
+        console.log("Humidity: ", humidity);
+        console.log("Clouds: ", WeatherData.daily[i].weather[0].description);
+    }
+}
+function addDays(date, days) {
+    var result = new Date(date);
+
+    result.setDate(result.getDate() + days);
+    console.log("Result Date: ", result);
+    return result;
+}
+function clear() {
+    $("#current-weathe").empty();
+}
+
+
+$("#run-search").on("click", function (event) {
+
+    event.preventDefault();
+    clear();
+
+    // Build the query URL for the ajax request to the NYT API
+    var queryURL = buildQueryURL();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (WeatherData) {
+        console.log(WeatherData);
+        console.log("------------------------------------");
+
+        var city = WeatherData.name;
+        console.log("City: ", city, "(", today,);
+        var lon = WeatherData.coord.lon;
+        console.log("longitude: ", lon);
+
+        var lat = WeatherData.coord.lat;
+        console.log("lattitude: ", lat);
+
+        console.log("------------------------------------");
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/onecall?";
+        var queryParams = { appid: "b0c91232e2976ef85bbb49769bd7a2b3" };
+        queryParams.lon = lon;
+        queryParams.lat = lat;
+
+        var queryURLall = queryURL + $.param(queryParams);
+        console.log("------------------------------------");
+        console.log("QueryURLall", queryURLall);
+
+        $.ajax({
+            url: queryURLall,
+            method: "GET"
+        }).then(updatePage)
+    });
+});
+
+    //  .on("click") function associated with the clear button
+    // $("#clear-all").on("click", clear)
